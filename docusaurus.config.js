129b4502e922753081bc8bddf65565e43567f89c
AMp <the.oppertunity@gmail.com>
2023-07-04 22:50:05 +0700

    Internal: Update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,8 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
+  trailingSlash: false,
+  
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'envirra', // Usually your GitHub org/user name.
